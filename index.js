7ee09078625c723e71d0a6a1bea48ad5af01c877
MuhammadRabius <muhammadrabiussany@gmail.com>
2022-05-22 23:27:31 +0600

    database connted

--- index.js
+++ index.js
@@ -7,6 +7,16 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json())
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://rhadmin:<password>@cluster0.5myll.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
 app.get('/', (req, res) => {
   res.send('Hello I m form Backend')
 })
