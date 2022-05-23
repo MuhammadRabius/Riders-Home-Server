94c1856a78d2200247ed3a8a21b046e6cf7e1ecd
MuhammadRabius <muhammadrabiussany@gmail.com>
2022-05-23 20:16:49 +0600

    colleection added and contected with database

--- index.js
+++ index.js
@@ -2,19 +2,37 @@ const express = require('express')
 const cors = require('cors');
 require('dotenv').config();
 const app = express();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json())
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://rhadmin:<password>@cluster0.5myll.mongodb.net/?retryWrites=true&w=majority";
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5myll.mongodb.net/?retryWrites=true&w=majority`;
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
+
+async function run(){
+       try{
+            await client.connect();
+            const partCollection = client.db('ridershome').collection('parts');
+
+            app.get('/parts',async(req,res)=>{
+              const query={}
+              const result = await partCollection.find(query).toArray();
+              res.send(result);
+              
+            })
+            
+            
+}
+    finally{
+
+    }
+}
+ run().catch(console.dir)
+;
 
 
 app.get('/', (req, res) => {
