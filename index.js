b3949bb5b728842eb1e9a9650e7099ad568872d2
MuhammadRabius <muhammadrabiussany@gmail.com>
2022-05-26 23:20:55 +0600

    payement api updated

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const app = express();
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
@@ -37,8 +38,9 @@ async function run(){
             const orderCollection = client.db('ridershome').collection('order');
             const userCollection = client.db('ridershome').collection('user');
             const reviewCollection = client.db('ridershome').collection('review');
-
-          const verifyAdmin = async (req, res, next) => {
+            const paymentCollection = client.db('ridershome').collection('payments');
+          
+            const verifyAdmin = async (req, res, next) => {
           const requester = req.decoded.email;
           const requesterAccount = await userCollection.findOne({ email: requester });
           if (requesterAccount.role === 'admin') {
@@ -79,6 +81,8 @@ async function run(){
             const parts = await partCollection.findOne(query);
             res.send(parts);
        })
+
+       app.
       // Place a order:POST
          app.post('/placeorder',async(req,res)=>{
               const newOrder = req.body;
@@ -187,7 +191,57 @@ async function run(){
       const result = await userCollection.deleteOne(filter);
       res.send(result);
     })
-          
+       
+    // payment section 
+
+    app.get('/payment-item/:id', async(req, res) =>{
+      const id = req.params.id;
+      console.log(id);
+      const query = {_id: ObjectId(id)};
+      const order = await orderCollection.findOne(query);
+      console.log(order);
+      res.send(order);
+    });
+
+    app.post('/create-payment-intent', async(req, res) =>{
+      const paymentOrder = req.body;
+      const price = paymentOrder.amount;
+      const newAmount = Number(price);
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount : newAmount,
+        currency: 'usd',
+        payment_method_types:['card']
+      });
+      res.send({clientSecret: paymentIntent.client_secret})
+    });
+
+    app.patch('/get-payment/:id',  async(req, res) =>{
+      const id  = req.params.id;
+      const payment = req.body;
+      const filter = {_id: ObjectId(id)};
+      const updatedDoc = {
+        $set: {
+          paid: true,
+          transactionId: payment.transactionId
+        }
+      }
+
+      const result = await paymentCollection.insertOne(payment);
+      const updatedOrder = await orderCollection.updateOne(filter, updatedDoc);
+      res.send(updatedOrder);
+    })
+// update parts
+    app.put('/updateparts/:partsName', async (req, res) => {
+      const name = req.params.partsName;
+      const filter = { partsName: name };
+      
+      const updateDoc = {
+       $inc: 
+                   { availableQuantity: -availableQuantity } 
+      };
+      const result = await partCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    })
       
             
             
